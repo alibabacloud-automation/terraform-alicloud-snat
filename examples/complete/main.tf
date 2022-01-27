85eb1398581efc1f55894259bfcc7642524f17e2
liuyuan <709503583@qq.com>
2022-01-27 18:48:58 +0800

    Improves the module examples/complete

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,41 +1,67 @@
-variable "region" {
-  default = "cn-hangzhou"
+data "alicloud_zones" "default" {
 }
 
-provider "alicloud" {
-  region = var.region
+data "alicloud_images" "default" {
+  name_regex = "ubuntu_18"
 }
 
-#############################################################
-# Data sources to get VPC and vswitch details
-#############################################################
-
-data "alicloud_vpcs" "default" {
-  is_default = true
+data "alicloud_instance_types" "default" {
+  availability_zone = data.alicloud_zones.default.zones.0.id
 }
 
-data "alicloud_vswitches" "default" {
-  ids = [data.alicloud_vpcs.default.vpcs.0.vswitch_ids[0]]
+module "vpc" {
+  source             = "alibaba/vpc/alicloud"
+  create             = true
+  vpc_cidr           = "172.16.0.0/12"
+  vswitch_cidrs      = [cidrsubnet("172.16.0.0/12", 8, 8), cidrsubnet("172.16.0.0/12", 8, 10), cidrsubnet("172.16.0.0/12", 8, 12), cidrsubnet("172.16.0.0/12", 8, 16), cidrsubnet("172.16.0.0/12", 8, 18)]
+  availability_zones = [data.alicloud_zones.default.zones.0.id]
 }
 
-data "alicloud_instances" "default" {
-  vpc_id = data.alicloud_vpcs.default.ids.0
+module "security_group" {
+  source = "alibaba/security-group/alicloud"
+  vpc_id = module.vpc.this_vpc_id
 }
-#############################################################
-# Using module to create a new nat gateway and bind two eip
-#############################################################
-module "nat" {
-  source = "terraform-alicloud-modules/nat-gateway/alicloud"
-  region = var.region
 
-  name   = "nat-foo"
-  vpc_id = data.alicloud_vpcs.default.ids.0
-  create = true
+module "ecs_instance" {
+  source = "alibaba/ecs-instance/alicloud"
 
-  // create eips and bind them with nat
-  create_eip    = true
-  number_of_eip = 4
-  eip_name      = "for-snat"
+  number_of_instances = 5
+
+  instance_type               = data.alicloud_instance_types.default.instance_types.0.id
+  image_id                    = data.alicloud_images.default.images.0.id
+  vswitch_ids                 = module.vpc.this_vswitch_ids
+  security_group_ids          = [module.security_group.this_security_group_id]
+  associate_public_ip_address = false
+  system_disk_category        = "cloud_ssd"
+  system_disk_size            = var.system_disk_size
+}
+
+resource "alicloud_nat_gateway" "this" {
+  vpc_id               = module.vpc.this_vpc_id
+  vswitch_id           = module.vpc.this_vswitch_ids[0]
+  nat_type             = var.nat_type
+  specification        = var.specification
+  payment_type         = "PayAsYouGo"
+  internet_charge_type = "PayByLcu"
+  period               = var.period
+}
+
+module "eip" {
+  source = "terraform-alicloud-modules/eip/alicloud"
+
+  create               = true
+  number_of_eips       = 5
+  bandwidth            = var.eip_bandwidth
+  internet_charge_type = "PayByTraffic"
+  instance_charge_type = "PostPaid"
+  period               = var.eip_period
+  isp                  = "BGP"
+}
+
+resource "alicloud_eip_association" "this" {
+  count         = 5
+  allocation_id = module.eip.this_eip_id[count.index]
+  instance_id   = alicloud_nat_gateway.this.id
 }
 
 ################################################
@@ -43,42 +69,56 @@ module "nat" {
 ################################################
 module "complete" {
   source = "../../"
-  region = var.region
 
-  create        = true
-  snat_table_id = module.nat.this_snat_table_id
+  create = true
 
-  # Default snat ip, which will be used for all snat entries.
-  snat_ips = module.nat.this_eip_ips
+  nat_gateway_id = alicloud_nat_gateway.this.id
+  snat_table_id  = alicloud_nat_gateway.this.snat_table_ids
+  snat_ips       = module.eip.this_eip_address
 
   # Open to CIDRs blocks
   snat_with_source_cidrs = [
     {
-      name         = "source-cidrs-for"
-      source_cidrs = join(",", [cidrsubnet(data.alicloud_vswitches.default.vswitches.0.cidr_block, 8, 10), cidrsubnet(data.alicloud_vswitches.default.vswitches.0.cidr_block, 8, 11)])
-      snat_ip      = module.nat.this_eip_ips[0]
-    },
-    {
-      name         = "source-cidrs-bar"
-      source_cidrs = cidrsubnet(data.alicloud_vswitches.default.vswitches.0.cidr_block, 8, 12)
-      snat_ip      = module.nat.this_eip_ips[1]
+      name         = var.name
+      source_cidrs = format("%s/32", module.ecs_instance.this_private_ip.0)
+      snat_ip      = module.eip.this_eip_address[0]
     }
   ]
 
   # Open for vswitch ids
   snat_with_vswitch_ids = [
     {
-      vswitch_ids = join(",", data.alicloud_vpcs.default.vpcs.0.vswitch_ids)
-      snat_ip     = module.nat.this_eip_ips[2]
+      name        = var.name
+      vswitch_ids = join(",", [module.vpc.this_vswitch_ids[1]])
+      snat_ip     = module.eip.this_eip_address[1]
     }
   ]
 
   # Open for ecs instance ids
   snat_with_instance_ids = [
     {
-      name         = "form-ecs"
-      instance_ids = join(",", data.alicloud_instances.default.ids)
-      snat_ip      = module.nat.this_eip_ips[3]
+      name         = var.name
+      instance_ids = join(",", [module.ecs_instance.this_instance_id[2]])
+      snat_ip      = module.eip.this_eip_address[2]
     }
   ]
+
+  # Open to computed CIDRs blocks
+  computed_snat_with_source_cidr = [
+    {
+      name        = var.name
+      source_cidr = format("%s/32", module.ecs_instance.this_private_ip.3)
+      snat_ip     = module.eip.this_eip_address[3]
+    }
+  ]
+
+  # Open for computed vswitch ids
+  computed_snat_with_vswitch_id = [
+    {
+      name       = var.name
+      vswitch_id = module.vpc.this_vswitch_ids[4]
+      snat_ip    = module.eip.this_eip_address[4]
+    }
+  ]
+
 }
---
+++ examples/complete/tfvars/01-update.tfvars
@@ -0,0 +1,13 @@
+#variables for ecs
+system_disk_size = 60
+
+#variables for nat gateway
+specification = "Middle"
+period        = 2
+
+# New EIP parameters
+eip_bandwidth = 10
+eip_period    = 2
+
+#variables for alicloud_snat_entry
+name = "update-tf-snat-name"
